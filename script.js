fe252f61d16e5fb62bb12af6848803e30d5b86c8
s1997aniket <52380306+s1997aniket@users.noreply.github.com>
2023-07-10 15:01:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Timer variables
-// Timer variables
-let workTime = parseInt(document.getElementById('session-length').value) * 60;
-let breakTime = parseInt(document.getElementById('break-length').value) * 60;
-let sessions = 2*parseInt(document.getElementById('session-count').value);
-
+let workTime = 25 * 60;
+let breakTime = 5 * 60;
+let sessions = 2 * 4;
 
 let timerInterval;
 let isPaused = true;
 let remainingSeconds;
-flag = 'Session';
+let flag = 'Session';
 
 // Update the timer display
 function updateTimerDisplay() {
@@ -44,23 +42,19 @@ function updateTime() {
   if (remainingSeconds < 0) {
     // Timer has reached zero
     pauseTimer();
-    if (sessions > 1 && flag == 'Session') {
+    if (sessions > 1 && flag === 'Session') {
       // Start break time
       sessions--;
       remainingSeconds = breakTime;
       alert('Time for a break!');
-      flag = 'Break'
-    } 
-
-    else if (sessions > 1 && flag == 'Break') {
-      // Start break time
+      flag = 'Break';
+    } else if (sessions > 1 && flag === 'Break') {
+      // Start work time
       sessions--;
       remainingSeconds = workTime;
-      alert('Time for a Session!');
-      flag = 'Session'
-    } 
-    
-    else {
+      alert('Time for a session!');
+      flag = 'Session';
+    } else {
       // All sessions completed
       remainingSeconds = 0;
       alert('All sessions completed!');
@@ -69,10 +63,19 @@ function updateTime() {
   updateTimerDisplay();
 }
 
+// Update timer settings and start timer
+function calculateTimer() {
+  workTime = parseInt(document.getElementById('session-length').value) * 60;
+  breakTime = parseInt(document.getElementById('break-length').value) * 60;
+  sessions = 2 * parseInt(document.getElementById('session-count').value);
+  resetTimer();
+}
+
 // Event listeners for buttons
 document.getElementById('start-btn').addEventListener('click', startTimer);
 document.getElementById('pause-btn').addEventListener('click', pauseTimer);
 document.getElementById('reset-btn').addEventListener('click', resetTimer);
+document.getElementById('calculate-btn').addEventListener('click', calculateTimer); // Add event listener for calculate button
 
 // Initial setup
 resetTimer();
